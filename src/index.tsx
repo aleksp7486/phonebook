e119f7361858d5bebb642adf8fa253b932fd67af
aleksp7486 <105385187+aleksp7486@users.noreply.github.com>
2023-05-14 12:43:19 +0300

    some fixes

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <BrowserRouter>
+      <BrowserRouter basename='/phonebook/'>
         <App />
       </BrowserRouter>
     </ChakraProvider>
